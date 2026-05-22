b262cdfaf51d276f5f64717a6a58b32e0a1c6f85
AbroRizwan1 <Rizwanabro14@gmail.com>
2026-05-22 17:48:20 +0500

    Fix CORS with correct frontend URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -26,12 +26,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: [
-      "https://melodia-music-web.vercel.app",
-      "http://localhost:5173",
-      "melodia-music-web-le3r.vercel.app",
-      "https://melodia-music-web-le3r-git-main-abrorizwan1s-projects.vercel.app",
-    ],
+    origin: true,
     credentials: true,
   }),
 );
