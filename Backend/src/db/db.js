f50604c29271f9362477447e606e8d86a52814fb
AbroRizwan1 <Rizwanabro14@gmail.com>
2026-05-23 13:20:38 +0500

    Fix MongoDB connection for Vercel

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 async function connectDB() {
+  if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
     console.log("DB connected successfully");
   } catch (error) {
-
     console.log("not Connected", error.message);
   }
 }
